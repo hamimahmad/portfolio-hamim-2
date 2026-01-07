c66cb961ac5b3097629b09a87fe1a92e324ba510
hamimahmad <hamimahmad027@gmail.com>
2026-01-08 00:14:32 +0600

    exp change

--- src/constants/index.js
+++ src/constants/index.js
@@ -243,27 +243,23 @@ export const mySocials = [
 
 export const experiences = [
   {
-    title: "Software Developer",
-    job: "Security & Defense Projects",
-    date: "2021-2023",
+    title: "Front-End Developer",
+    job: "Consumberbase",
+    date: "2023-2024",
     contents: [
-      "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
-      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
-      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
-      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
-      "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
+      "Theme Development: Building custom, responsive themes and child themes that adhere to WordPress Coding Standards.",
+      "Design Translation: Converting Figma or Adobe XD mockups into semantic HTML, CSS/Sass, and modern JavaScript.",
+      "Performance Optimization: Ensuring high Core Web Vitals scores through image optimization, minification, and efficient asset loading.",
+      "Cross-Browser Testing: Ensuring consistent visual appearance and functionality across all modern browsers and mobile devices.",
+      "Maintenance: Troubleshooting UI bugs and ensuring frontend compatibility during WordPress core or plugin updates.",
     ],
   },
   {
-    title: "Back-End Developer",
-    job: "Car Manufacture",
-    date: "2023-2024",
+    title: "Front-End Developer",
+    job: "UVTR Info Tech Solutions",
+    date: "2024-2025",
     contents: [
-      "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
-      "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
-      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
-      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
-      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+      "Collaborate with web designers and back-end developers to complete projects Create wireframes and mockups of site/application designs Turn feedback into custom solutions for client needs Optimize sites/apps to improve performance and efficiency Author technical documentation.",
     ],
   },
   {
